b815e5da166ee56deb8f53f74535cd3de2e33239
Lukasz Tymoszczuk <publicperson@gmail.com>
2026-05-20 14:38:20 +0100

    clean up

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,9 @@ import neostandard from 'neostandard'
 
 export default neostandard({
   env: ['node', 'vitest'],
+  globals: {
+    fetchMock: 'readonly'
+  },
   ignores: [...neostandard.resolveIgnoresFromGitignore()],
   noJsx: true,
   noStyle: true
